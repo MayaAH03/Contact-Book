c84fe74ba9cef7f9abee4d73d5eeba94496141a4
Maya Hourani <alhomaya735@gmail.com>
2024-07-24 02:25:46 +0300

    Configured app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +15,20 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 #db is data base
 db = SQLAlchemy(app)
 
+#describing the path to the frontend and the dist folder(the one that contains the final product). getcwd just means get current working directory.
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+#this line means that the basic route/url (localhost5000) (the one that doesnt include /api/friends), will take us to the react frontend component as the landing page of that url (localhost5000).
+#server static files from the dist folder under the "frontend" directory.
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
+#api routes
 import routes #dont need the from in the beginning because routes doesnt return anything
 
 with app.app_context(): #this line is just for optimization
